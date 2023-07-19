57ffe89677428dfb634a3080af4a5c7a4c497147
ngocquy032 <112050702+ngocquy032@users.noreply.github.com>
2023-07-19 18:15:52 +0700

    update img boostrap

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,19 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import Layout from "@/layouts/Layout.vue"
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: Layout,
+    children: [
+      {
+        path: '/',
+        component: ()=> import('@/views/Home.vue')
+
+      }
+    ]
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+
 ]
 
 const router = createRouter({
