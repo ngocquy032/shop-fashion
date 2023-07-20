d9ad0e450ba35762d2137e37c7b0e58faa9f94f6
ngocquy032 <112050702+ngocquy032@users.noreply.github.com>
2023-07-20 18:12:51 +0700

    Layout

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,17 @@
+<template>
+    <Header/>
+   
+    <router-view></router-view>
+    
+  <Footer/>
+</template>
+
+<script setup lang="ts">
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+
+</script>
+
+<style scoped>
+
+</style>
